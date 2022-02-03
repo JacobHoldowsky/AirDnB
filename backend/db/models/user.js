bba5b18df22c8a8613543a53b3f3c01f2debd363
Jacob Holdowsky <jacobedward1995@gmail.com>
2022-02-03 16:54:04 -0500

    set up user auth middlewares - end of phase 3

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -2,19 +2,16 @@
 const { Validator } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
-
-
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true,
       validate: {
-        len: [4, 30],
+        len: [3, 30],
         isNotEmail(value) {
           if (Validator.isEmail(value)) {
-            throw new Error('Cannot be an email.')
+            throw new Error('Cannot be an email.');
           }
         }
       }
@@ -27,27 +24,29 @@ module.exports = (sequelize, DataTypes) => {
       }
     },
     hashedPassword: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING.BINARY,
       allowNull: false,
       validate: {
         len: [60, 60]
       }
-    },
-    defaultScope: {
-      attributes: {
-        exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
-      }
-    },
-    scopes: {
-      currentUser: {
-        attributes: { exclude: ['hashedPassword'] }
-      },
-      loginUser: {
-        attributes: {}
-      }
     }
-  });
-
+  },
+    {
+      defaultScope: {
+        attributes: {
+          exclude: ['hashedPassword', 'email', 'createdAt', 'updatedAt']
+        }
+      },
+      scopes: {
+        currentUser: {
+          attributes: { exclude: ['hashedPassword'] }
+        },
+        loginUser: {
+          attributes: {}
+        }
+      }
+    });
+    
   User.prototype.toSafeObject = function () { // remember, this cannot be an arrow function
     const { id, username, email } = this; // context will be the User instance
     return { id, username, email }
