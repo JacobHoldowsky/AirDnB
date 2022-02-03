08bc0bd46fc7a4d4661bc7514bfe8fcc1667f448
Jacob Holdowsky <jacobedward1995@gmail.com>
2022-02-03 18:02:50 -0500

    handleValidationErrors written

---
+++ backend/utils/validation.js
@@ -0,0 +1,24 @@
+const { validatonResult } = require('express-validator');
+
+// middleware for formatting errors from express-validator middleware
+// (to customize, see express-validator's documentation)
+const handleValidationErrors = (Req, _res, next) => {
+    const validationErrors = validatonResult(req);
+
+    if (!validationErrors.isEmpty()) {
+        const errors = validationErrors
+            .array()
+            .map((error) => `${error.msg}`);
+
+        const err = Error('Bad request.');
+        err.errors = errors;
+        err.status = 400;
+        err.title = 'Bad request.';
+        next(err);
+    }
+    next();
+};
+
+module.exports = {
+    handleValidationErrors
+}
