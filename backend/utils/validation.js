02de696ad31faa68e74c41f14e485ea31b5ce01b
Jacob Holdowsky <jacobedward1995@gmail.com>
2022-02-03 18:37:19 -0500

    backend complete

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -1,9 +1,9 @@
-const { validatonResult } = require('express-validator');
+const { validationResult } = require('express-validator');
 
 // middleware for formatting errors from express-validator middleware
 // (to customize, see express-validator's documentation)
-const handleValidationErrors = (Req, _res, next) => {
-    const validationErrors = validatonResult(req);
+const handleValidationErrors = (req, _res, next) => {
+    const validationErrors = validationResult(req);
 
     if (!validationErrors.isEmpty()) {
         const errors = validationErrors
