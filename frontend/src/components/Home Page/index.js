b5bdd26c6e52c8041dfc617bde27d77476e40376
Jacob Holdowsky <jacobedward1995@gmail.com>
2022-02-11 12:04:00 -0500

    worked on landing page

--- frontend/src/components/Home Page/index.js
+++ frontend/src/components/Home Page/index.js
@@ -4,9 +4,12 @@ const HomePage = () => {
 
     return(
         <div className={'home-page-container'}>
-            <div className={'welcome-blurb'}>
+            <h1 className={'welcome-blurb'}>
                 Welcome to AirDnB!
-            </div>
+            </h1>
+            <h2 className={'where-will-you-go-next'}>
+                Where will you go next?
+            </h2>
         </div>
         
     )
